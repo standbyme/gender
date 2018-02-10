584d28d997a9ed1ef5d5e469c0060a52818be223
TGS <tgs@tgs.blue>
2018-02-10 22:22:04 +0800

    add BaiduAPI

--- src/from_pic_url.ts
+++ src/from_pic_url.ts
@@ -7,4 +7,16 @@ export default async function (pic_url: string) {
         encoding: 'base64',
     }
     const img_base64 = await request(options)
+
+    // tslint:disable-next-line:variable-name
+    const AipFaceClient = require('baidu-aip-sdk').face
+
+    const APP_ID = '10818581'
+    const API_KEY = 'V9n4YrySvBpdiWZOzNt5FGe9'
+    const SECRET_KEY = 'gTfso0G0yzKcZA01WhbH6KQzeLguI9o9'
+
+    const client = new AipFaceClient(APP_ID, API_KEY, SECRET_KEY)
+
+    const result = await client.detect(img_base64, { face_fields: 'gender' })
+    console.log(result)
 }
