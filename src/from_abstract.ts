4e7aeea2b9b6956991d0ed93dccc6bdbf77a79f2
TGS <tgs@tgs.blue>
2018-02-10 23:22:02 +0800

    Finish

--- src/from_abstract.ts
+++ src/from_abstract.ts
@@ -1,11 +1,10 @@
 import Gender from '@enum-all/gender'
 import { Option } from 'funfix-core'
-import * as _ from 'lodash'
-
 import { gender_string_enum__map } from './string_enum__map'
 import { gender_lang_to_gender_enum } from './utility'
 
 const gender = [/，(男|女)，/, /^(男|女)，/, /：(男|女)，/]
+const _head = require('lodash.head');
 
 function reg_match(reg: RegExp, str: string): Option<RegExpExecArray> {
     return Option.of(reg.exec(str))
@@ -13,5 +12,5 @@ function reg_match(reg: RegExp, str: string): Option<RegExpExecArray> {
 
 export default function (abstract: string): Option<Gender> {
     const gender__list = gender.map(reg => reg_match(reg, abstract)).filter(v => v.nonEmpty()).map(v => v.get())
-    return Option.of(_.head(gender__list)).map(reev => reev[1]).chain(gender_lang_to_gender_enum)
+    return Option.of(_head(gender__list)).map(reev => reev[1]).chain(gender_lang_to_gender_enum)
 }
