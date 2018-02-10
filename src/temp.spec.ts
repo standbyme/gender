89e464820989c6ec458c145474c2a6b616d9dff3
TGS <tgs@tgs.blue>
2018-02-10 20:29:09 +0800

    init

---
+++ src/temp.spec.ts
@@ -0,0 +1,10 @@
+import * as assert from 'assert'
+
+describe('Get HTML', function () {
+    this.slow(5000)
+    this.timeout(10000)
+    it('should return None if url is not of baike content', async function () {
+    })
+    it('should return Some if url is of baike content', async function () {
+    })
+})
