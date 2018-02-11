1ad86b35cf9f716bf8dd7e040c0cd5c83a83358b
TGS <tgs@tgs.blue>
2018-02-11 20:00:09 +0800

    fix gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,23 +4,25 @@ let ts = require('gulp-typescript')
 let mocha = require('gulp-mocha')
 
 const tsProject = ts.createProject('tsconfig.json')
-
+const tsResult = tsProject.src()
+    .pipe(tsProject())
+    .on('error', function () {
+        done('TS compile fail')
+    })
 
 gulp.task('clean', function () {
     return del(['dist/*'])
 })
 
-gulp.task('compile', gulp.series('clean', function compile_func(done) {
-    const tsResult = tsProject.src()
-        .pipe(tsProject())
-        .on('error', function () {
-            done('TS compile fail')
-        })
+gulp.task('dts', function () {
+    return tsResult.dts.pipe(gulp.dest('dist'))
+})
 
-    tsResult.js.pipe(gulp.dest('dist'))
-    tsResult.dts.pipe(gulp.dest('dist'))
-    return tsResult
-}))
+gulp.task('js', function () {
+    return tsResult.js.pipe(gulp.dest('dist'))
+})
+
+gulp.task('compile', gulp.series('clean', 'js','dts'))
 
 gulp.task('test', gulp.series('compile', function test_func() {
     return gulp.src(['dist/**/**.spec.js'], { read: false })
